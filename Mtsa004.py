19bb7d6ded8f4abad003d3445a08947a5641b9f1
Santuse <benb2053@gmail.com>
2022-02-09 16:00:07 -0600

    attempt 2

--- Mtsa004.py
+++ Mtsa004.py
@@ -1,5 +1,26 @@
-import py_compile
-
-
 class Mtsa004(object):
-    def find_mtsa(self, A, B)
+    def find_mtsa(self, A: list[int], B: list[int]):
+
+
+        if len(B) > len(A): #A always bigger
+            A, B = B, A
+        saver = k = len(B)//2
+        q = len(A)//2
+        total = len(A) + len(B)
+    
+
+        while k > 0:
+            k+=1
+            q = total - saver
+            if B[saver] > A[q]:
+                saver-=k
+                print("B>A -", saver, k)
+            elif B[saver] < A[q]:
+                saver+=k
+                print("B<A +", saver, k)
+            else:
+                k = 0
+            k=k//2
+
+        if saver == 0:
+
